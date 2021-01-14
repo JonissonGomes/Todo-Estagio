1e44c108813b6cbc00db135fd11ce48ec73483f1
JonissonGomes <joni_gomes@hotmail.com>
2021-01-14 19:24:51 -0300

    Criando rota para os metodos implementados

--- todo/app/urls.py
+++ todo/app/urls.py
@@ -1,9 +1,13 @@
 # Importando dependencias
 from django.contrib import admin
 from django.urls import path
+
+# Importando views
 from app.views import todo_list
+from app.views import todo_detail_change_and_delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', todo_list)
+    path('', todo_list),
+    path('<int:pk>/', todo_detail_change_and_delete),
 ]
