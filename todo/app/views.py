e74f3d332af10731182e8c301c5c6879bc1c518a
JonissonGomes <joni_gomes@hotmail.com>
2021-01-14 19:50:06 -0300

    Refatorando os metodos HTTP

--- todo/app/views.py
+++ todo/app/views.py
@@ -1,6 +1,8 @@
 # Importar dependencias
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 
 # Importar models
@@ -9,42 +11,43 @@ from app.models import Todo
 # Importar serializers
 from app.serializers import TodoSerializer
 
-#Criar função para request GET e POST
+# Criando métodos de requisição(Refatorando)
 
-@api_view(['GET','POST'])
 
-def todo_list(request):
-    if request.method == 'GET':
-        todo = Todo.objects.all() # Retorna todos os objetos do model
-        serializer = TodoSerializer(todo, many=True) # Compila todos os dados do banco em formato JsonResponse
+class TodoListAndCreate(APIView):
+    def get_object(self, pk):
+        try:
+            return Todo.objects.get(pk=pk)  # Irá buscar as chaves primearias no banco
+        except Todo.DoesNotExist:
+           raise NotFound()
+
+    # Método GET
+    def get(self, request, pk):
+        todo = self.get_object(pk)
+        todo = Todo.objects.all()  # Retorna todos os objetos do model
+        serializer = TodoSerializer(todo, many=True) # Compila todos os dados do banco em formato Json
         return Response(serializer.data) # Retorna os dados do banco em formato Json
-    elif request.method == 'POST':
-        serializer = TodoSerializer(data=request.data) # Envia todos os dados requeridos no request
+
+    # Método POST
+    def post(self, request,pk):
+        todo = self.get_object(pk)
+        serializer = TodoSerializer(data=request.data)
         if serializer.is_valid(): # Faz a validação dos dados requeridos no request
             serializer.save() # Salva os dados do request
             return Response(serializer.data, status=status.HTTP_201_CREATED) # Retorna Sucesso no request
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # Retorna Erro na requisição dos dados
-    
 
-@api_view(['GET','PUT','DELETE'])
-
-def todo_detail_change_and_delete(request, pk):
-    try:
-        todo = Todo.objects.get(pk=pk) # Irá buscar as chaves primearias no banco
-    except Todo.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-        serializer = TodoSerializer(todo) # Irá armazenar a chave primaria em um objeto JSON
-        return Response(serializer.data) # Irá retornar os dados referentes aos do banco
-    
-    elif request.method == 'PUT':
+    # Método PUT
+    def put(self, request, pk):
+        todo = self.get_object(pk)
         serializer = TodoSerializer(todo, data=request.data)  # Ira armazenar as 2 chaves(1 do banco e outra do request) em formato JSON
         if serializer.is_valid(): # Comparar os dados para validação
             serializer.save() # Salva as alterações
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND) # Retorna mensagem de erro
-    
-    elif request.method == 'DELETE':
+
+    # Método DELETE
+    def delete(self, request, pk):
+        todo = self.get_object(pk)
         todo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
