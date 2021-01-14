f933b770601897a51cb1cbb8f3a76b7244b8901c
JonissonGomes <joni_gomes@hotmail.com>
2021-01-14 18:33:06 -0300

    Criando model Todo

--- todo/app/models.py
+++ todo/app/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class Todo(models.Model):
+    name = models.CharField(max_length=200)
+    stack = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    time = models.IntegerField()
+    done = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True)
