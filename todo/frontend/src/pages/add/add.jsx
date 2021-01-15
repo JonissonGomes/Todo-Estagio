700d4578e9116d0028b7d5cc3c38d7494f03f05c
JonissonGomes <joni_gomes@hotmail.com>
2021-01-15 16:36:23 -0300

    Adicionando componentes a tela

--- todo/frontend/src/pages/add/add.jsx
+++ todo/frontend/src/pages/add/add.jsx
@@ -1,13 +1,38 @@
-import './add.css';
+import "./add.css";
+import { Link } from "react-router-dom";
 
+// Importando imagens e icones
+import imageAddTodo from "../../assets/Add-notes-bro.svg";
 
-function Add(){
-    return (
-        <div>
-            <h2>Sou o Add</h2>
-        </div>
-    );
+function Add() {
+  return (
+    <div className="Container">
+      <Link to="/">
+        <p className="btn-close">Voltar</p>
+      </Link>
+      <img src={imageAddTodo} alt="New-Taks" />
 
+      <h1> Criar tarefa</h1>
+
+      <section className="boxForm">
+        <form action="">
+          <label htmlFor="Name">Nome</label>
+          <input type="text" name="name" id="Name" />
+
+          <label htmlFor="Stack">Stack</label>
+          <input type="text" name="stack" id="Stack" />
+
+          <label htmlFor="Description">Descrição</label>
+          <input type="text" name="description" id="Description" />
+          <input type="submit" value="Criar" />
+        </form>
+      </section>
+
+      <Link to="/">
+        <button className="btn-back"> Voltar </button>
+      </Link>
+    </div>
+  );
 }
 
 export default Add;
