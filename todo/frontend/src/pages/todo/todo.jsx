15725b78f217448fab9c8c0a0f0fb70b8e44ab19
JonissonGomes <joni_gomes@hotmail.com>
2021-01-16 02:52:41 -0300

    Criando page view Todo

--- todo/frontend/src/pages/todo/todo.jsx
+++ todo/frontend/src/pages/todo/todo.jsx
@@ -1,13 +1,85 @@
-import './todo.css';
+import { Link } from 'react-router-dom';
+
+import "./todo.css";
+
+import imageTask from "../../assets/Task-bro.svg";
+
+function Todo() {
+  return (
+    <div className="Container">
+        <Link to="/">
+        <p className="btn-close">Voltar</p>
+      </Link>
+      <img src={imageTask} alt="Task" />
+      <h1>To-Do List</h1>
+      <section className="boxContainer">
+
+        
+        <section className="boxSection">
+          <section className="boxContent">
+            <h3>Criar API </h3>
+            <p className="Stack">Python</p>
+            <p className="Description">
+              Criar uma API para utilizar como backend do projeto.
+            </p>
+          </section>
+
+          <section className="Dias">
+            <label htmlFor="">Dias</label>
+            <h2>3</h2>
+          </section>
+
+          <section className="Status">
+            <label htmlFor="">Status</label>
+            <input type="checkbox" name="done" id="Done" checked />
+          </section>
+        </section>
+
+        
+        <section className="boxSection">
+          <section className="boxContent">
+            <h3>Construir frontend React</h3>
+            <p className="Stack">JavaScript</p>
+            <p className="Description">
+              Criar as telas utilizando o framework React.
+            </p>
+          </section>
+
+          <section className="Dias">
+            <label htmlFor="">Dias</label>
+            <h2>1</h2>
+          </section>
+
+          <section className="Status">
+            <label htmlFor="">status</label>
+            <input type="checkbox" name="done" id="Done" />
+          </section>
+        </section>
+
+        <section className="boxSection">
+          <section className="boxContent">
+            <h3>Documentação do projeto</h3>
+            <p className="Stack">Dev</p>
+            <p className="Description">
+              Detalhar a criação e funcionamento da API e criar manual de execução do front end
+            </p>
+          </section>
+
+          <section className="Dias">
+            <label htmlFor="">Dias</label>
+            <h2>3</h2>
+          </section>
+
+          <section className="Status">
+            <label htmlFor="">Status</label>
+            <input type="checkbox" name="done" id="Done" />
+          </section>
+        </section>
 
 
-function Todo(){
-    return (
-        <div>
-            <h2>Sou o Todo</h2>
-        </div>
-    );
-
+      </section>
+    </div>
+  );
 }
 
 export default Todo;
