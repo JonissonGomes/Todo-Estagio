0a35b9bbf16c8861d6bfc4ce073b51fba2509e85
JonissonGomes <joni_gomes@hotmail.com>
2021-01-16 15:06:48 -0300

    Importando dependencias e criando stados para implementaçao dos dados

--- todo/frontend/src/pages/todo/todo.jsx
+++ todo/frontend/src/pages/todo/todo.jsx
@@ -1,80 +1,52 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import api from "../../api";
 
 import "./todo.css";
 
 import imageTask from "../../assets/Task-bro.svg";
 
 function Todo() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    api.get("todo/").then((taskList) => {
+      setTasks(taskList.data);
+    });
+  }, []);
+
   return (
     <div className="Container">
-        <Link to="/">
+      <Link to="/">
         <p className="btn-close">Voltar</p>
       </Link>
       <img src={imageTask} alt="Task" />
       <h1>To-Do List</h1>
       <section className="boxContainer">
 
-        
-        <section className="boxSection">
+       {tasks.map((task)=>{
+         return (
+          <section className="boxSection">
           <section className="boxContent">
-            <h3>Criar API </h3>
-            <p className="Stack">Python</p>
+            <h3>{task.name}</h3>
+            <p className="Stack">{task.stack}</p>
             <p className="Description">
-              Criar uma API para utilizar como backend do projeto.
+              {task.description}
             </p>
           </section>
 
           <section className="Dias">
             <label htmlFor="">Dias</label>
-            <h2>3</h2>
+            <h2>{task.time}</h2>
           </section>
 
           <section className="Status">
             <label htmlFor="">Status</label>
-            <input type="checkbox" name="done" id="Done" checked />
+            <input type="checkbox" name="done" id="Done" checked={task.done}/>
           </section>
         </section>
-
-        
-        <section className="boxSection">
-          <section className="boxContent">
-            <h3>Construir frontend React</h3>
-            <p className="Stack">JavaScript</p>
-            <p className="Description">
-              Criar as telas utilizando o framework React.
-            </p>
-          </section>
-
-          <section className="Dias">
-            <label htmlFor="">Dias</label>
-            <h2>1</h2>
-          </section>
-
-          <section className="Status">
-            <label htmlFor="">status</label>
-            <input type="checkbox" name="done" id="Done" />
-          </section>
-        </section>
-
-        <section className="boxSection">
-          <section className="boxContent">
-            <h3>Documentação do projeto</h3>
-            <p className="Stack">Dev</p>
-            <p className="Description">
-              Detalhar a criação e funcionamento da API e criar manual de execução do front end
-            </p>
-          </section>
-
-          <section className="Dias">
-            <label htmlFor="">Dias</label>
-            <h2>3</h2>
-          </section>
-
-          <section className="Status">
-            <label htmlFor="">Status</label>
-            <input type="checkbox" name="done" id="Done" />
-          </section>
-        </section>
+         )
+       })}
 
 
       </section>
